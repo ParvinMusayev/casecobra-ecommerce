368e8ff6b95c79014f0d4bf53ac9fd902f15d9b4
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-19 13:14:45 +0400

    feat: Add icons component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Check, Star } from "lucide-react";
 // components
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Icons } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -110,7 +111,12 @@ export default function Home() {
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
-              What our <span className="relative px-2">customers </span> say
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{" "}
+              say
             </h2>
             <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
           </div>
