146cd62451be3ea01687903e24a75dd4ab739c45
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-22 00:40:38 +0400

    feat: Add steps component

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,10 +1,13 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-
 import { ReactNode } from "react";
+
+// components
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Steps from "@/components/Steps";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
       {children}
     </MaxWidthWrapper>
   );
