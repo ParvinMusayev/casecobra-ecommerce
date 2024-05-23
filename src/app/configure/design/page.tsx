472ad841c53ec57fab4bea3f6e4d782beff9d85d
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-24 00:30:08 +0400

    chore: update design page

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,28 @@
-const Page = async () => {
-  return <p></p>;
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
 };
 
 export default Page;
