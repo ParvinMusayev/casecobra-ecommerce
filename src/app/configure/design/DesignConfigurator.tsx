c98ee2ac6d07a5a49b3240897203f0a5c9872cc0
ParvinMusayev <parvinmusayev1@gmail.com>
2024-05-25 13:47:53 +0400

    feat: Add label

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -8,6 +8,8 @@ import HandleComponent from "@/components/HandleComponent";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { RadioGroup } from "@headlessui/react";
 import { useState } from "react";
+import { COLORS } from "@/validators/option-validator";
+import { Label } from "@/components/ui/label";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -21,8 +23,11 @@ const DesignConfigurator = ({
   imageDimensions,
 }: DesignConfiguratorProps) => {
   const [options, setOptions] = useState<{
-    color: "black";
-  }>;
+    color: (typeof COLORS)[number];
+  }>({
+    color: COLORS[0],
+  });
+
   return (
     <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
@@ -96,7 +101,9 @@ const DesignConfigurator = ({
                       color: val,
                     }));
                   }}
-                ></RadioGroup>
+                >
+                  <Label>Color: {options.color.label}</Label>
+                </RadioGroup>
               </div>
             </div>
           </div>
