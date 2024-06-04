9d47a2e3dbfbdb006b45aa9b3e5a9d869b12dae0
ParvinMusayev <parvinmusayev1@gmail.com>
2024-06-04 23:48:43 +0400

    chore: update design preview

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Phone from "@/components/Phone";
+import { cn } from "@/lib/utils";
+import { COLORS } from "@/validators/option-validator";
 import { Configuration } from "@prisma/client";
 import { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
@@ -7,6 +10,12 @@ import Confetti from "react-dom-confetti";
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
   useEffect(() => setShowConfetti(true));
+
+  const { color, model, finish, material } = configuration;
+
+  const tw = COLORS.find(
+    (supportedColor) => supportedColor.value === color
+  )?.tw;
   return (
     <>
       <div
@@ -17,6 +26,15 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
           active={showConfetti}
           config={{ elementCount: 200, spread: 90 }}
         />
+      </div>
+
+      <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
+        <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
+          <Phone
+            className={cn(`bg-${tw}`, "max-w-[150px] md:max-w-full")}
+            imgSrc={configuration.croppedImageUrl!}
+          />
+        </div>
       </div>
     </>
   );
