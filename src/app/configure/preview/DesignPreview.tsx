117101bebff4523e3476bd9656585b653f48df01
ParvinMusayev <parvinmusayev1@gmail.com>
2024-06-04 23:42:27 +0400

    chore: update deisgn preview

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -1,9 +1,25 @@
 "use client";
 
 import { Configuration } from "@prisma/client";
+import { useEffect, useState } from "react";
+import Confetti from "react-dom-confetti";
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
-  return <div></div>;
+  const [showConfetti, setShowConfetti] = useState<boolean>(false);
+  useEffect(() => setShowConfetti(true));
+  return (
+    <>
+      <div
+        aria-hidden="true"
+        className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center"
+      >
+        <Confetti
+          active={showConfetti}
+          config={{ elementCount: 200, spread: 90 }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default DesignPreview;
