337b64edce1b2eedbca2df6a225d04fdc4789401
ParvinMusayev <parvinmusayev1@gmail.com>
2024-06-05 10:59:18 +0400

    feat: Add actions

---
+++ src/app/configure/preview/actions.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export const createCheckoutSession = async ({
+  configId,
+}: {
+  configId: string;
+}) => {
+  const configuration = await db.configuration.findUnique({
+    where: { id: configId },
+  });
+
+  if (!configuration) {
+    throw new Error("No such configuration found");
+  }
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+};
