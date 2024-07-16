61b03c738eeeaa279a279cdb3f4d369bbb6ed0d7
ParvinMusayev <parvinmusayev1@gmail.com>
2024-07-16 20:10:52 +0400

    chore: update thank you page

---
+++ src/app/thank-you/ThankYou.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getPaymentStatus } from "./actions";
+import { useSearchParams } from "next/navigation";
+import { Loader2 } from "lucide-react";
+
+const ThankYou = () => {
+  const searchParams = useSearchParams();
+  const orderId = searchParams.get("orderId") || "";
+
+  const { data } = useQuery({
+    queryKey: ["get-payment-status"],
+    queryFn: async () => await getPaymentStatus({ orderId }),
+    retry: true,
+    retryDelay: 500,
+  });
+
+  if (data === undefined) {
+    return (
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+          <h3 className="font-semibold text-xl">Loading your order...</h3>
+          <p>This won't take long.</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (data === false) {
+    return (
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+          <h3 className="font-semibold text-xl">Verifying your payment...</h3>
+          <p>This might take a moment.</p>
+        </div>
+      </div>
+    );
+  }
+
+  return <div></div>;
+};
+
+export default ThankYou;
