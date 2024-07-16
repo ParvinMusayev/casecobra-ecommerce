1b88ec13936bf48dc21ef096e1be48b2337c8d4f
ParvinMusayev <parvinmusayev1@gmail.com>
2024-07-16 19:57:05 +0400

    chore: update router

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/db";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
@@ -22,6 +23,49 @@ export async function POST(req: Request) {
       if (!event.data.object.customer_details?.email) {
         throw new Error("Missing user email");
       }
+
+      const session = event.data.object as Stripe.Checkout.Session;
+
+      const { userId, orderId } = session.metadata || {
+        userId: null,
+        orderId: null,
+      };
+
+      if (!userId || !orderId) {
+        throw new Error("Invalid request metadata");
+      }
+
+      const billingAddress = session.customer_details!.address;
+      const shippingAddress = session.shipping_details!.address;
+
+      const updatedOrder = await db.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          isPaid: true,
+          shippingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: shippingAddress!.city!,
+              country: shippingAddress!.country!,
+              postalCode: shippingAddress!.postal_code!,
+              street: shippingAddress!.line1!,
+              state: shippingAddress!.state,
+            },
+          },
+          billingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: billingAddress!.city!,
+              country: billingAddress!.country!,
+              postalCode: billingAddress!.postal_code!,
+              street: billingAddress!.line1!,
+              state: billingAddress!.state,
+            },
+          },
+        },
+      });
     }
   } catch (err) {
     console.log(err);
