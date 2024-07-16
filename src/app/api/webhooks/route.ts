72787ef8314ab09ec7877fd88707844b702adf82
ParvinMusayev <parvinmusayev1@gmail.com>
2024-07-16 19:55:02 +0400

    chore: update router

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,7 +1,28 @@
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import Stripe from "stripe";
 
 export async function POST(req: Request) {
   try {
+    const body = await req.text();
+
+    const signature = headers().get("stripe-signature");
+
+    if (!signature) {
+      return new Response("Invalid signature", { status: 400 });
+    }
+
+    const event = Stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+
+    if (event.type === "checkout.session.completed") {
+      if (!event.data.object.customer_details?.email) {
+        throw new Error("Missing user email");
+      }
+    }
   } catch (err) {
     console.log(err);
 
