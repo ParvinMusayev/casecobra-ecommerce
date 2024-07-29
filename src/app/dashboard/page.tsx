0c3b2169ab923dd8c9be7739433daae1811a5528
ParvinMusayev <parvinmusayev1@gmail.com>
2024-07-29 14:19:56 +0400

    chore: update Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,60 @@
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound } from "next/navigation";
 
 const Page = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+
+  const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+
+  if (!user || user.email !== ADMIN_EMAIL) {
+    return notFound();
+  }
+
+  const orders = await db.order.findMany({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      user: true,
+      shippingAddress: true,
+    },
+  });
+
+  const lastWeekSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const lastMonthSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 30)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  });
+
+  const WEEKLY_GOAL = 500;
+  const MONTHLY_GOAL = 2500;
+
   return (
     <div className="flex min-h-screen w-full bg-muted/40">
       secret dashboard page
